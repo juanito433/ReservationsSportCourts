8b188f7101e0e917138820f5f8b1b2ce9e882099
Juan <lopezsurianjuancarlos5@gmail.com>
2025-03-13 12:49:29 -0600

    login con alerta de bienvenida

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, Image } from 'react-native';
+import { View, Text, TextInput, Button, Alert, StyleSheet, Image, useWindowDimensions } from 'react-native';
 import axios from 'axios';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [message, setMessage] = useState(''); // Estado para el mensaje
+    const [message, setMessage] = useState('');
+
+    const { width } = useWindowDimensions(); // Detecta el ancho de la pantalla
 
     const handleLogin = async () => {
         try {
@@ -14,11 +16,9 @@ const LoginScreen = () => {
                 password,
             });
 
-            console.log('Respuesta del servidor:', response.data);
             if (response.data.status === 200) {
                 setMessage(`Bienvenido, ${response.data.member.name}`);
                 Alert.alert('Éxito', `Bienvenido, ${response.data.member.name}`);
-
             } else {
                 setMessage('Inicio de sesión fallido.');
             }
@@ -29,25 +29,35 @@ const LoginScreen = () => {
     };
 
     return (
-        <View style={styles.container}>
-            <Image style={styles.image} source={require('../assets/image/haciendas.png')} />
-            <Text style={styles.title}>Iniciar Sesión</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Correo electrónico"
-                keyboardType="email-address"
-                value={email}
-                onChangeText={setEmail}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Contraseña"
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-            />
-            <Button title="Iniciar Sesión" onPress={handleLogin} />
-            {message ? <Text style={styles.message}>{message}</Text> : null}
+        <View style={[styles.container, width > 768 ? styles.webContainer : null]}>
+            {width > 768 && (
+                <View style={styles.logoContainer}>
+                    <Image style={styles.image} source={require('../assets/image/login.jpg')} />
+                </View>
+            )}
+
+            <View style={styles.formContainer}>
+                {width <= 768 && (
+                    <Image style={styles.image} source={require('../assets/image/login.jpg')} />
+                )}
+                <Text style={styles.title}>Iniciar Sesión</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Correo electrónico"
+                    keyboardType="email-address"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+                <Button title="Iniciar Sesión" onPress={handleLogin} />
+                {message ? <Text style={styles.message}>{message}</Text> : null}
+            </View>
         </View>
     );
 };
@@ -58,12 +68,27 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 20,
     },
+    webContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 50,
+    },
+    logoContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    formContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        maxWidth: 768, 
+    },
     image: {
-        width: 300,
+        width: 200,
         height: 200,
-
+        borderRadius: 100,
+        marginBottom: 25,
         alignSelf: 'center',
-        marginBottom: 20,
     },
     title: {
         fontSize: 24,
